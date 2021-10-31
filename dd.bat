f845d1fdfedb1ce8af076b13621ac045dd8b3fb5
zijia <ilraygo@gmail.com>
2021-10-31 11:26:03 +0800

    update

--- dd.bat
+++ dd.bat
@@ -1,13 +1,13 @@
 @echo off
-            Echo 正在检查系统中是否存在木马
-            Netstat -a -n >jc.txt
-            Find jc.txt "7614">nul
-            If %errorlevel%==0(
-            Echo.
-            Echo 系统中存在木马
-            Goto end
-            )
-            Echo.
-            Echo 系统中未发现木马
-            :end
-            Del /q jc.txt>nul
+    Echo 正在检查系统中是否存在木马
+    Netstat -a -n >jc.txt
+    Find jc.txt "7614">nul
+    If %errorlevel%==0(
+    Echo.
+    Echo 系统中存在木马
+    Goto end
+    )
+    Echo.
+    Echo 系统中未发现木马
+    :end
+    Del /q jc.txt>nul
